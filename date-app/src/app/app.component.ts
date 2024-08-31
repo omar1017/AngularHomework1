d53b32e290d6a74b9149f2edac3ada4b4a9b8e5f
omar1017 <omarnhnh@outlook.sa>
2024-08-31 15:39:54 +0300

    import libs to support ar

--- date-app/src/app/app.component.ts
+++ date-app/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NgModel } from '@angular/forms';
+import { LOCALE_ID } from '@angular/core';
+import { DatePipe, registerLocaleData } from '@angular/common';
+import localeAr from '@angular/common/locales/ar';
+import { CommonModule } from '@angular/common';
+import { pipe } from 'rxjs';
+
+registerLocaleData(localeAr);
+
+
+
+
 @Component({
   selector: 'app-root',
+  providers: [{ provide: LOCALE_ID, useValue: 'ar' }],
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
